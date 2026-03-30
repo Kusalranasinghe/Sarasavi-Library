ebe37353cc27a2736ca096c636db3601ee0ea8b4
Sathira Sugeesvara <sathirasugeesvara@gmail.com>
2026-03-30 23:49:54 +0530

    Revamp admin UI, add pages and DB schema updates

---
+++ includes/footer.php
@@ -0,0 +1,44 @@
+<footer id="footer" class="footer">
+    <div class="footer-container">
+
+        <div class="footer-col">
+            <h2>Main Office</h2>
+            <p>456 Main Street,<br>
+                Kaluthara Road,<br>
+                Matugama,<br>
+                Sri Lanka.
+            </p>
+        </div>
+
+        <div class="footer-col">
+            <h2>Contact Numbers</h2>
+            <ul>
+                <li>+94 22 123 4567</li>
+                <li>+94 11 999 8765</li>
+            </ul>
+        </div>
+
+        <div class="footer-col">
+            <h2>Quick Links</h2>
+            <ul>
+                <li><a href="privacy.php">Privacy Policy</a></li>
+                <li><a href="terms.php">Terms of Service</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-col">
+            <h2>Follow Us</h2>
+            <ul>
+                <li><a href="#"><i class='bx bxl-facebook'></i> Facebook</a></li>
+                <li><a href="#"><i class='bx bxl-twitter'></i> Twitter</a></li>
+                <li><a href="#"><i class='bx bxl-instagram'></i> Instagram</a></li>
+                <li><a href="#"><i class='bx bxl-gmail'></i> Gmail</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-bottom">
+            <p>© 2026 Sarasavi Library | All Rights Reserved</p>
+        </div>
+
+    </div>
+</footer>
